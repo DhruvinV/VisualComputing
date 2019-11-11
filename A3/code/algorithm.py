428b11698bc7b6361ce64babbd84778a1a717c5d
Dhruvin Vekariya <39427569+DhruvinV@users.noreply.github.com>
2019-11-10 19:23:19 -0500

    som

--- A3/code/algorithm.py
+++ A3/code/algorithm.py
@@ -124,8 +124,7 @@ def propagation_and_random_search(source_patches, target_patches,
                         while( k < int(np.ceil(- np.log10(w)/ np.log10(alpha)))):
                             Rx = np.random.uniform(-1,1)
                             Ry = np.random.uniform(-1,1)
-                            u = f[i,j] + np.multiply(w*(a**k),np.array(Rx,Ry)
-                            # x,y = i + u[0], j+u[1]
+                            u = f[i,j] + np.multiply(w*(a**k),np.array(Rx,Ry))
                             if(within_dim((i+u[0], j+u[1]),target_patches)==False):
                                 new_score = np.linalg.norm(source_patches[i,j]-target_patches[i+u[0],j+u[1]])
                                 if(new_dist < best_D[i,j]):
@@ -165,7 +164,7 @@ def propagation_and_random_search(source_patches, target_patches,
                         while( k < int(np.ceil(- np.log10(w)/ np.log10(alpha)))):
                             Rx = np.random.uniform(-1,1)
                             Ry = np.random.uniform(-1,1)
-                            u = f[i,j] + np.multiply(w*(a**k),np.array(Rx,Ry)
+                            u = f[i,j] + np.multiply(w*(a**k),np.array(Rx,Ry))
                             # x,y = i + u[0], j+u[1]
                             if(within_dim((i + u[0], j+u[1]),target_patches)==False):
                                 new_score = np.linalg.norm(source_patches[i,j]-target_patches[i+u[0],j+u[1]])
